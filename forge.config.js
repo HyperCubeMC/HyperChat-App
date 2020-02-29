a0ec269b2b9bd512613818ad67a196d2c5e7f764
Justsnoopy30 <33374736+Justsnoopy30@users.noreply.github.com>
2020-02-29 17:19:09 -0600

    Add the actual project

---
+++ forge.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  packagerConfig: {
+    "icon": "./src/favicon.ico"
+  },
+  "make_targets": {
+    "win32": ["squirrel"], // An array of win32 make targets
+    "darwin": ["zip", "dmg"], // An array of darwin make targets
+    "linux": ["deb", "rpm", "flatpak", "snap"] // An array of linux make targets
+  },
+  makers: [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "name": "HyperChat"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-dmg",
+      "config": {}
+    },
+    {
+      "name": "@electron-forge/maker-rpm",
+      "config": {}
+    }
+  ]
+}
