16826b7f47b51b616ae39af6624dd29ee953a03a
Justsnoopy30 <everettallen30@gmail.com>
2020-03-05 18:46:03 -0600

    Add initial titlebar change api

--- src/preload.js
+++ src/preload.js
@@ -1,17 +1,19 @@
 const { contextBridge, ipcRenderer } = require('electron')
 const customTitlebar = require('custom-electron-titlebar');
-
-contextBridge.exposeInMainWorld(
-  'electron',
-  {
-    setWindowTitleBarColor: (setWindowTitleBarColor) => {
-      new customTitlebar.Titlebar({
-        backgroundColor: customTitlebar.Color.fromHex(setWindowTitleBarColor)
-      });
-      ipcRenderer.send('setWindowTitleBarColor', setWindowTitleBarColor)
-    },
-    customApi: (customApi) => {
-      ipcRenderer.send('customApi', customApi)
+window.onload = function () {
+  const titlebar = new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex("#FFF")
+  });
+  contextBridge.exposeInMainWorld(
+    'electron',
+    {
+      setWindowTitleBarColor: (setWindowTitleBarColor) => {
+        titlebar.updateBackground(customTitlebar.Color.fromHex(setWindowTitleBarColor));
+        ipcRenderer.send('setWindowTitleBarColor', setWindowTitleBarColor)
+      },
+      customApi: (customApi) => {
+        ipcRenderer.send('customApi', customApi)
+      }
     }
-  }
-)
+  )
+}
