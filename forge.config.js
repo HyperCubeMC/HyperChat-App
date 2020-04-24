d1e06e33c55375e03b35fe77cf3deedede0a348c
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-04-23 19:54:05 -0500

    Fix asset linking and update upstream.

--- forge.config.js
+++ forge.config.js
@@ -1,7 +1,7 @@
 var packageJSON = require('./package.json');
 module.exports = {
   "packagerConfig": {
-    "icon": "./page/favicon"
+    "icon": "./page/assets/favicon"
   },
   "make_targets": {
     "win32": ["squirrel"],
@@ -13,7 +13,7 @@ module.exports = {
       "name": "@electron-forge/maker-squirrel",
       "config": {
         "setupExe": `${packageJSON.productName}-${packageJSON.version}-Setup.exe`,
-        "setupIcon": "./page/favicon.ico"
+        "setupIcon": "./page/assets/favicon.ico"
       }
     },
     {
