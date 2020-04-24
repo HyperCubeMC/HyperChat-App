1df3f665e0de41d7139fbc0740dea1621955e788
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-04-23 21:43:08 -0500

    Remove electron devtools installer.

--- app/init.js
+++ app/init.js
@@ -31,12 +31,6 @@ app.on('ready', () => {
   // ipcMain.on('setWindowTitleBarColor', (event, arg) => {
   //   console.log("Set title bar color: " + arg);
   // })
-  const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-
-  installExtension(REACT_DEVELOPER_TOOLS)
-      .then((name) => console.log(`Added Extension:  ${name}`))
-      .catch((err) => console.log('An error occurred: ', err));
-
   createWindow();
 });
 
